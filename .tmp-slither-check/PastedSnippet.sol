1f19d6673b504bbf138350467b20f9c0ce5a04d0
Dev-In-Crypt <cryptodevonblock@gmail.com>
2026-03-04 21:02:41 +0100

    chore: ignore and untrack temporary slither artifacts

--- .tmp-slither-check/PastedSnippet.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity 0.8.24;
-contract X {
-  function pay(address payable to) external payable {
-    (bool ok, ) = to.call{value: msg.value}("");
-    require(ok, "send failed");
-  }
-}
